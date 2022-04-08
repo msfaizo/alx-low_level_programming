c7187c0dd32560c4ac6d54ea85a1215c3a14eb71
msfaizo <faizosinani@gmail.com>
2022-04-08 09:41:02 +0300

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
- 
 /**
- * main - Entry point
- *
- * Return: Aways 0 (Success)  
+ * main - program entry point
+ * Return: 0
  */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
-}
+} 
