f279ab658781a7e90747e57d6bdb9c49fe932027
msfaizo <faizosinani@gmail.com>
2022-04-08 09:42:55 +0300

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main - program entry point
+ * main - main block or program entry point
  * Return: 0
  */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
-} 
+}
