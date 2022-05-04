3f5ce4131309ae4291ebd72153cdd97890dde143
msfaizo <faizosinani@gmail.com>
2022-05-05 00:20:27 +0300

    malloc free

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,16 +1,17 @@
 #include "main.h"
 /**
-* _puts - print a string followed by a new line
-*@str: string to print
-*Return: Nothing
-*/
+ * _puts - print a string followed by a new line
+ *@str: string to print
+ *Return: void
+ */
 void _puts(char *str)
 {
-	int i;
+	int i = 0;
 
-	for (i = 0; *(str + i) != 0; i++)
+	while (*(str + i))
 	{
-		_putchar(str[i]);
+		_putchar(*(str + i));
+		i++;
 	}
 	_putchar('\n');
 }
