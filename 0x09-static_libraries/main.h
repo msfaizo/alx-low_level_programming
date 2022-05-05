bac9da0bdd3868d992e56c618257a02b0c1badcc
msfaizo <faizosinani@gmail.com>
2022-05-05 08:44:59 +0300

    Static libraries

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,7 +1,5 @@
-#ifndef MAIN_FUNC
-#define MAIN_FUNC
-
-#include <stdio.h>
+#ifndef MAIN_H
+#define MAIN_H
 
 int _putchar(char c);
 int _islower(int c);
@@ -24,4 +22,4 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 
-#endif /* MAIN_FUNC */
+#endif /* MAIN_H */
